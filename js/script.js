b63978b586d2450df9701e05ddaf904014a2f6c2
Andrew T <at100991@student.sgh.waw.pl>
2020-12-23 15:01:43 +0200

    Create modal window

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+let modal = document.querySelector("#myModal");
+
+let btn = document.querySelector('.add-btn');
+
+let span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
